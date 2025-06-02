7404069b082ba088fd81f5812f91e6e077d76c34
tdeepak383 <tdeepak383@gmail.com>
2025-06-02 22:47:30 +0530

    coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>Deepak Kumar Portfolio Coming Soon...</h1>
+      <h1 className='text-4xl font-bold'>Deepak Kumar Portfolio Coming Soon...</h1>
     </>
   )
 }
